a1abd27cd6803ea91d3c940e8bd0cc206efd9ec6
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-02-25 00:27:59 -0300

    fix(Usuario): Decorator perdido num mock removido

--- src/JuliusReport/usuario/__mocks__/usuario.model.ts
+++ src/JuliusReport/usuario/__mocks__/usuario.model.ts
@@ -1,49 +1,26 @@
-import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Column, Index, OneToMany } from "typeorm";
-import { ApiModelProperty } from "@nestjs/swagger";
 import * as bcrypt from 'bcryptjs';
 import { Veiculo } from "../../../JuliusReport/veiculo/veiculo.model";
 
-@Entity()
 export class Usuario {
 
-    @PrimaryGeneratedColumn()
-    @ApiModelProperty()
     id: number;
 
-    @CreateDateColumn()
-    @ApiModelProperty()
     dataregistro: Date;
 
-    @UpdateDateColumn()
-    @ApiModelProperty()
     atualizadoem: Date;
 
-    @Column()
-    @ApiModelProperty()
     nome: string;
 
-    @Column()
-    @ApiModelProperty()
     email: string;
 
-    @Column( { nullable: true } )
-    @ApiModelProperty()
     telefone: string;
 
-    @Column( { nullable: true } )
-    @ApiModelProperty()
     endereco: string;
 
-    @Column()
-    @Index( { unique: true } )
-    @ApiModelProperty()
     login: string;
 
-    @Column()
-    @ApiModelProperty()
     private passwordHash: string;
 
-    @OneToMany( type => Veiculo, veiculos => Veiculo )
     veiculos: Veiculo[];
 
     veiculoDefault: Veiculo; // campo que recebe o veículo na seção
