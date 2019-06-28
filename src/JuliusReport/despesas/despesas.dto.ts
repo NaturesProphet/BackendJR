d9575fb7159a0bebf6c3b47d86c9612fb5134a93
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-06-28 15:37:37 -0300

    test(Despesa): teste e2e da rota POST /despesa preparado.

--- src/JuliusReport/despesas/despesas.dto.ts
+++ src/JuliusReport/despesas/despesas.dto.ts
@@ -1,12 +1,19 @@
+import { ApiModelProperty } from "@nestjs/swagger";
+
 export class DespesaDto {
 
+  @ApiModelProperty()
   tipo: 'imposto' | 'taxa' | 'multa';
 
+  @ApiModelProperty()
   dataPagamento: Date;
 
+  @ApiModelProperty()
   descricao: string;
 
+  @ApiModelProperty()
   valorTotal: number;
 
+  @ApiModelProperty()
   veiculo_id: number;
 }
---
+++ test/despesa.controller.e2e-spec.ts
@@ -0,0 +1,189 @@
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { Test, TestingModule } from "@nestjs/testing";
+import * as request from 'supertest';
+import { INestApplication } from "@nestjs/common";
+import { AppModule } from "../src/app.module";
+import { loginPayload } from "../src/JuliusReport/login/loginPayload.dto";
+const feature = loadFeature( "./test/features/despesas.feature" );
+
+jest.mock( "../src/JuliusReport/despesas/despesas.model" );
+jest.mock( "../src/JuliusReport/julius-report.module" );
+jest.mock( "../src/JuliusReport/usuario/usuario.model" );
+jest.mock( "../src/JuliusReport/veiculo/veiculo.model" );
+
+
+
+defineFeature( feature, test => {
+    let module: TestingModule;
+    let app: INestApplication;
+
+    beforeAll( async () => {
+        module = await Test.createTestingModule( {
+            imports: [ AppModule ]
+        } ).compile();
+        app = module.createNestApplication();
+        await app.init();
+    } );
+
+
+
+
+    test( '1: Os dados informados são válidos', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova despesa do meu veículo', () => {
+            endpoint = '/despesa';
+        } );
+
+        and( 'possuo um token de acesso válido', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'existente1';
+            payload.senha = 'test@123***';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            body = {
+                dataPagamento: new Date(),
+                descricao: 'despesa de teste',
+                tipo: 'taxa',
+                valorTotal: 0.01,
+                veiculo_id: 1
+            }
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de confirmação com um código 201 na Resposta', () => {
+            expect( response.status ).toBe( 201 );
+            expect( response.text ).toBe( 'Despesa registrada' );
+        } );
+    } );
+
+
+
+    test( '2: Os dados informados NÃO são válidos.', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova despesa do meu veículo', () => {
+            endpoint = '/despesa';
+        } );
+
+        and( 'possuo um token de acesso válido', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'existente1';
+            payload.senha = 'test@123***';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        and( 'Digitei algum valor inválido no cadastro', () => {
+            body = {
+                dataPagamento: null,
+                descricao: 'despesa de teste',
+                tipo: 'taxa',
+                valorTotal: null,
+                veiculo_id: 1
+            }
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de erro com um código 400 na Resposta', () => {
+            expect( response.status ).toBe( 400 );
+            expect( response.text ).toBe( 'Dados inválidos' );
+        } );
+    } );
+
+
+
+    test( '3: O veículo informado não pertence ao usuário autenticado.', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova despesa de um veículo', () => {
+            endpoint = '/despesa';
+        } );
+
+        and( 'possuo um token de acesso válido', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'existente1';
+            payload.senha = 'test@123***';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        and( 'este veículo não me pertence', () => {
+            body = {
+                dataPagamento: new Date(),
+                descricao: 'despesa de teste',
+                tipo: 'taxa',
+                valorTotal: 0.01,
+                veiculo_id: 69
+            }
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de erro com um código 403 na Resposta', () => {
+            expect( response.status ).toBe( 403 );
+            expect( response.text ).toBe( 'O veiculo buscado não pertence ao usuário especificado na consulta' );
+        } );
+    } );
+
+
+
+    test( '4: O usuário NÃO está autenticado', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova despesa do meu veículo', () => {
+            endpoint = '/despesa';
+        } );
+
+        and( 'não possuo um token de acesso válido', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'WindowsEstavel';
+            payload.senha = 'muitoprovavel';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            body = {
+                dataPagamento: new Date(),
+                descricao: 'despesa de teste',
+                tipo: 'taxa',
+                valorTotal: 0.01,
+                veiculo_id: 1
+            }
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de erro com um código 401 na Resposta', () => {
+            expect( response.status ).toBe( 401 );
+            expect( response.text ).toBe( 'Não autenticado' );
+        } );
+    } );
+
+
+
+    afterAll( async () => {
+        await app.close();
+    } );
+
+} );
