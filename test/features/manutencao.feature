183e477f1998a89d63761c66648d75733f847e71
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-06-29 16:40:12 -0300

    test(Manutencao): Teste e2e para a rota POST /manutencao preparado

--- test/features/manutencao.feature
+++ test/features/manutencao.feature
@@ -1,27 +1,36 @@
 #language: pt
 Funcionalidade: Cadastrar novas manutenções de um veículo
-Endpoint que cadastra novas manutenções para o veículo na seção
+Endpoint que cadastra novas manutenções para um veículo
 
-Cenário: Os dados informados são válidos, o usuário está autenticado e o veículo está na seção.
-Dado Quero registrar uma nova manutenção do meu veículo
+Cenário: 1: Os dados informados são válidos
+Dado Quero registrar uma nova manutenção de um veículo
+E Tenho um token de acesso válido
 Quando eu enviar os dados de registro
-Então recebo uma mensagem de confirmação com um código 200 na Resposta
+Então recebo uma mensagem de confirmação com um código 201 na Resposta
 
-
-Cenário: Os dados informados são válidos, o usuário está autenticado mas o veículo não está na seção.
-Dado Quero registrar uma nova manutenção do meu veículo
-E ainda não inicializei a seção com um veículo
+Cenário: 2: Os dados informados NÃO são válidos
+Dado Quero registrar uma nova manutenção de um veículo
+E Tenho um token de acesso válido
+E Esqueci de preencher algum dado ou digitei errado
 Quando eu enviar os dados de registro
 Então recebo uma mensagem de erro com um código 400 na Resposta
 
-Cenário: O usuário está autenticado na seção mas o veículo informado não pertence a ele.
-Dado Quero registrar uma nova manutenção para um veículo
-E este veículo não me pertence
+Cenário: 3: O Veículo informado não pertence ao usuário que enviou a requisição
+Dado Quero registrar uma nova manutenção de um veículo
+E Tenho um token de acesso válido
+E informei o ID de um veículo que não me pertence
 Quando eu enviar os dados de registro
 Então recebo uma mensagem de erro com um código 403 na Resposta
 
+Cenário: 4: O Veículo informado não existe no banco de dados
+Dado Quero registrar uma nova manutenção de um veículo
+E Tenho um token de acesso válido
+E informei o ID de um veículo que não existe no banco de dados
+Quando eu enviar os dados de registro
+Então recebo uma mensagem de erro com um código 400 na Resposta
 
-Cenário: O usuário NÃO está autenticado na seção
-Dado Quero registrar uma nova manutenção do meu veículo
+Cenário: 5: O usuário não está autenticado
+Dado Quero registrar uma nova manutenção de um veículo
+E Não possuo um token de acesso válido ou ele já expirou
 Quando eu enviar os dados de registro
 Então recebo uma mensagem de erro com um código 401 na Resposta
---
+++ test/manutencao.controller.e2e-spec.ts
@@ -0,0 +1,221 @@
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { Test, TestingModule } from "@nestjs/testing";
+import * as request from 'supertest';
+import { INestApplication } from "@nestjs/common";
+import { AppModule } from "../src/app.module";
+import { loginPayload } from "../src/JuliusReport/login/loginPayload.dto";
+
+const feature = loadFeature( "./test/features/manutencao.feature" );
+
+jest.mock( "../src/JuliusReport/manutencao/manutencao.model" );
+jest.mock( "../src/JuliusReport/julius-report.module" );
+jest.mock( "../src/JuliusReport/usuario/usuario.model" );
+jest.mock( "../src/JuliusReport/veiculo/veiculo.model" );
+
+
+defineFeature( feature, test => {
+    let module: TestingModule;
+    let app: INestApplication;
+
+    beforeAll( async () => {
+        module = await Test.createTestingModule( {
+            imports: [ AppModule ]
+        } ).compile();
+        app = module.createNestApplication();
+        await app.init();
+    } );
+
+    test( '1: Os dados informados são válidos', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova manutenção de um veículo', () => {
+            endpoint = '/manutencao';
+        } );
+
+        and( 'Tenho um token de acesso válido', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'donoDaFrota123';
+            payload.senha = 'test@123***';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            body = {
+                dataInicio: new Date(),
+                dataFinal: new Date(),
+                descricao: 'Serviço de teste',
+                local: 'Oficina do Tião Lanterneiro',
+                valorMaterial: 100.00,
+                valorServico: 80.00,
+                veiculo_id: 54321
+            }
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de confirmação com um código 201 na Resposta', () => {
+            expect( response.status ).toBe( 201 );
+            expect( response.text ).toBe( 'Manutenção registrada' );
+        } );
+    } );
+
+
+    test( '2: Os dados informados NÃO são válidos', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova manutenção de um veículo', () => {
+            endpoint = '/manutencao';
+        } );
+
+        and( 'Tenho um token de acesso válido', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'donoDaFrota123';
+            payload.senha = 'test@123***';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        and( 'Esqueci de preencher algum dado ou digitei errado', () => {
+            body = {
+                dataInicio: new Date(),
+                dataFinal: null,
+                descricao: 'Serviço de teste',
+                local: 'Oficina do Tião Lanterneiro',
+                valorMaterial: null,
+                valorServico: 80.00,
+                veiculo_id: 54321
+            }
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de erro com um código 400 na Resposta', () => {
+            expect( response.status ).toBe( 400 );
+            expect( response.text ).toBe( 'Os dados enviados são inválidos' );
+        } );
+    } );
+
+
+    test( '3: O Veículo informado não pertence ao usuário que enviou a requisição', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova manutenção de um veículo', () => {
+            endpoint = '/manutencao';
+        } );
+
+        and( 'Tenho um token de acesso válido', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'donoDaFrota123';
+            payload.senha = 'test@123***';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        and( 'informei o ID de um veículo que não me pertence', () => {
+            body = {
+                dataInicio: new Date(),
+                dataFinal: new Date(),
+                descricao: 'Serviço de teste',
+                local: 'Oficina do Tião Lanterneiro',
+                valorMaterial: 100.00,
+                valorServico: 80.00,
+                veiculo_id: 1
+            }
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de erro com um código 403 na Resposta', () => {
+            expect( response.status ).toBe( 403 );
+            expect( response.text ).toBe( 'O veiculo informado não pertence ao usuário' );
+        } );
+    } );
+
+
+    test( '4: O Veículo informado não existe no banco de dados', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova manutenção de um veículo', () => {
+            endpoint = '/manutencao';
+        } );
+
+        and( 'Tenho um token de acesso válido', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'donoDaFrota123';
+            payload.senha = 'test@123***';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        and( 'informei o ID de um veículo que não existe no banco de dados', () => {
+            body = {
+                dataInicio: new Date(),
+                dataFinal: new Date(),
+                descricao: 'Serviço de teste',
+                local: 'Oficina do Tião Lanterneiro',
+                valorMaterial: 100.00,
+                valorServico: 80.00,
+                veiculo_id: 0
+            }
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de erro com um código 400 na Resposta', () => {
+            expect( response.status ).toBe( 400 );
+            expect( response.text ).toBe( 'O veículo informado não existe' );
+        } );
+    } );
+
+
+    test( '5: O usuário não está autenticado', ( { given, and, when, then } ) => {
+        let endpoint: string;
+        let body: any;
+        let response: any;
+        let token: any;
+        given( 'Quero registrar uma nova manutenção de um veículo', () => {
+            endpoint = '/manutencao';
+        } );
+
+        and( 'Não possuo um token de acesso válido ou ele já expirou', async () => {
+            let payload: loginPayload = new loginPayload();
+            payload.login = 'WindowsTrabalhaMuitoBemSim';
+            payload.senha = 'éVerdadeEsseBilete';
+            token = await request( app.getHttpServer() ).post( '/login' ).send( payload );
+            token = token.text;
+        } );
+
+        when( 'eu enviar os dados de registro', async () => {
+            response = await request( app.getHttpServer() ).post( endpoint )
+                .send( body ).set( { authorization: `Bearer ${token}` } );
+        } );
+
+        then( 'recebo uma mensagem de erro com um código 401 na Resposta', () => {
+            expect( response.status ).toBe( 401 );
+            expect( response.text ).toBe( 'Não autenticado' );
+        } );
+    } );
+
+    afterAll( async () => {
+        await app.close();
+    } );
+
+} );
